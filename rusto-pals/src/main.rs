b151f7ec9f370bec262fc3ba407916918800f437
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-14 14:36:40 -0400

    challenge 3 (manual check for "english-like" string)

--- rusto-pals/src/main.rs
+++ rusto-pals/src/main.rs
@@ -10,9 +10,14 @@ fn main() {
     }
     
     {
-        let input1 = "1c0111001f010100061a024b53535009181c";
-        let input2 = "686974207468652062756c6c277320657965";
-        println!("{}", challenge2(input1, input2));
+        // let input1 = "1c0111001f010100061a024b53535009181c";
+        // let input2 = "686974207468652062756c6c277320657965";
+        // println!("{}", challenge2(input1, input2));
+    }
+
+    {
+        let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        println!("{}", challenge3(input));
     }
 }
 
@@ -28,4 +33,23 @@ fn challenge2(input1: &str, input2: &str) -> String {
             ch1 ^ ch2
         })
         .collect()).unwrap().as_bytes().to_hex()
+}
+
+fn challenge3(input: &str) -> String {
+    for x in 0..256 {
+        let temp = String::from_utf8(
+            input.from_hex().unwrap().iter().map(
+                |&ch| ch ^ (x as u8)
+            ).collect());
+
+        let ans = match temp {
+            Ok(result) => result, // println!("{}: {}", x, result),
+            Err(error) => "".to_string() // println!("{}: {}", x, error)
+        };
+
+        // manually examined println output to determine index
+        if x == 120 { return ans };
+    }
+
+    unreachable!()
 }
